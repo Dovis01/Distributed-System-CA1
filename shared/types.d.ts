a5633e56aee334051b39c2428c0ca4eea3f7830b
Shijin Zhang <1364226740@qq.com>
2024-03-06 05:06:07 +0800

    Fix-the problem for ignoring the types declaration file on git.

---
+++ shared/types.d.ts
@@ -0,0 +1,23 @@
+export type MovieReview = {
+    MovieId: number;
+    ReviewerName: string;
+    ReviewDate: string;
+    Content: string;
+    Rating: number;
+}
+
+export type SignUpBody = {
+    username: string;
+    password: string;
+    email: string;
+}
+
+export type ConfirmSignUpBody = {
+    username: string;
+    code: string;
+}
+
+export type SignInBody = {
+    username: string;
+    password: string;
+}
