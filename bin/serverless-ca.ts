16e469e0b9033a47645872b2f422554c6f4cfc91
Shijin Zhang <1364226740@qq.com>
2024-03-07 05:38:52 +0800

    Improve-the way and structure of creating multiple stacks.

--- bin/serverless-ca.ts
+++ bin/serverless-ca.ts
@@ -2,7 +2,23 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { InitConfigStack } from "../lib/serverless-ca/init-config-stack";
+import {AuthApiStack} from "../lib/serverless-ca/auth-api-stack";
+import {AppApiStack} from "../lib/serverless-ca/app-api-stack";
 
 const app = new cdk.App();
-new InitConfigStack(app, "InitConfigStack", { env: { region: "eu-west-1" } });
 
+const initConfigStack = new InitConfigStack(app, "InitConfigStack", { env: { region: "eu-west-1" } });
+
+new AuthApiStack(app, 'AuthApiStack', {
+    userPoolId: initConfigStack.userPool.userPoolId,
+    userPoolClientId: initConfigStack.appClient.userPoolClientId,
+    lambdaLayer: initConfigStack.lambdaLayer,
+    env: { region: 'eu-west-1' },
+});
+
+new AppApiStack(app, 'AppApiStack', {
+    userPoolId: initConfigStack.userPool.userPoolId,
+    userPoolClientId: initConfigStack.appClient.userPoolClientId,
+    lambdaLayer: initConfigStack.lambdaLayer,
+    env: { region: 'eu-west-1' },
+});
--- lib/serverless-ca/init-config-stack.ts
+++ lib/serverless-ca/init-config-stack.ts
@@ -1,51 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import {UserPool} from "aws-cdk-lib/aws-cognito";
-import {AuthApiStack} from "./auth-api-stack";
-import {AppApiStack} from "./app-api-stack";
+import {UserPool, UserPoolClient} from "aws-cdk-lib/aws-cognito";
 
 export class InitConfigStack extends cdk.Stack {
+    public readonly lambdaLayer: lambda.LayerVersion;
+    public readonly userPool: UserPool;
+    public readonly appClient: UserPoolClient;
+
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // Add the Lambda layer
-        const lambdaLayer = new lambda.LayerVersion(this, 'LambdaLayer', {
+        this.lambdaLayer = new lambda.LayerVersion(this, 'LambdaLayer', {
             code: lambda.Code.fromAsset('layers'),
             compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
             description: 'A layer that contains the nodejs packages',
         });
 
-
         // Create the Cognito User Pool
-        const userPool = new UserPool(this, "UserPool", {
-            signInAliases: { username: true, email: true },
+        this.userPool = new UserPool(this, "UserPool", {
+            signInAliases: {username: true, email: true},
             selfSignUpEnabled: true,
             removalPolicy: cdk.RemovalPolicy.DESTROY,
         });
 
-        const appClient = userPool.addClient("AppClient", {
-            authFlows: { userPassword: true },
+        // Add Cognito App Client
+        this.appClient = this.userPool.addClient("AppClient", {
+            authFlows: {userPassword: true},
         });
-
-        const userPoolId = userPool.userPoolId;
-        const userPoolClientId = appClient.userPoolClientId;
-
-
-        // Pass parameters to the Auth and App API Stack
-        new AuthApiStack(this, 'AuthApiStack', {
-            userPoolId: userPoolId,
-            userPoolClientId: userPoolClientId,
-            lambdaLayer: lambdaLayer,
-            env: { region: "eu-west-1" }
-        });
-
-        new AppApiStack(this, 'AppApiStack', {
-            userPoolId: userPoolId,
-            userPoolClientId: userPoolClientId,
-            lambdaLayer: lambdaLayer,
-            env: { region: "eu-west-1" }
-        } );
 
     }
 }
